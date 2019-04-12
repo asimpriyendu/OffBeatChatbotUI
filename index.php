e0ef6bd625f21db8a58be314dc5720fea7700dd4
Asim Priyendu <priyenduasim@gmail.com>
2019-04-12 17:13:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -261,7 +261,7 @@
       document.getElementById("myForm").style.display = "none";
       document.getElementById('response').innerHTML = ' ';
       document.getElementById('container').setAttribute('class', null);
-      document.getElementById("myForm").reset();
+      document.getElementById("input").reset();
       if(responsiveVoice.isPlaying()) 
       {
         responsiveVoice.cancel();
